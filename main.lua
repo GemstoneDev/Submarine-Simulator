f5422f476ea32b514ae28a13e82c4723c84b2798
Steven <80566162+BreakSapphire@users.noreply.github.com>
2022-07-01 14:19:52 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,65 @@
+--[[
+    Come here and take my stuff skids
+]]
+-- //UI Colors\\ --
+_G.ButtonColor = Color3.fromRGB(0, 132, 255)
+
+-- //Variables\\ --
+local fakejumpscare = game:GetService("StarterGui").night.jumpscare
+local realjumpscare = game:GetService("StarterGui").gameover.jumpscare
+
+fakeJSSound = game:GetService("Players").LocalPlayer.PlayerGui.night.LocalScript.Jumpscare
+realJSSound = game:GetService("Players").LocalPlayer.PlayerGui.night.LocalScript.realjumpscare
+
+-- //Welcoming Notification\\ --
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Submarine Simulator Script";
+    Text = "Made by Gemstone#8299 (me)";
+    Duration = 120;
+    Button1 = "Okay";
+})
+wait(0.5)
+-- //UILib\\ --
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/BreakSapphire/Wally-V3-Backup/main/script/main.lua')))()
+
+local MainWindow = library:CreateWindow("Main")
+
+local LocalPlayer = MainWindow:CreateFolder("LocalPlayer")
+
+LocalPlayer:Button("Infinite Power",function()
+    local pmt = getrawmetatable(game);
+        make_writeable(pmt);
+        local pold_index = pmt.__index;
+
+        pmt.__index = function(a, b)
+            if tostring(a) == "Power" then
+                if tostring(b) == "Value" then
+                return math.huge 
+            end
+        end
+        return pold_index(a, b)
+    end
+end)
+
+LocalPlayer:Button("Infinite Oxygen",function()
+    local omt = getrawmetatable(game);
+    make_writeable(omt);
+    local oold_index = omt.__index;
+
+    omt.__index = function(a, b)
+        if tostring(a) == "Oxygen" then
+            if tostring(b) == "Value" then
+            return math.huge 
+            end
+        end
+        return oold_index(a, b)
+    end
+end)
+
+local Him = MainWindow:CreateFolder("\"Him\"")
+Him:Button("Remove Jumpscare", function()
+    fakejumpscare.Image = nil
+    realjumpscare.Image = nil
+    fakeJSSound.SoundId = nil
+    realJSSound.SoundId = nil
+end)
